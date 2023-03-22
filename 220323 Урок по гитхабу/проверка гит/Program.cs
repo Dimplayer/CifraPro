7b128eaea73b32948c1bb38816983e53141a93ce
Dimplayer <Dimacoolin@gmail.com>
2023-03-22 19:44:07 +0200

    проверка тест

---
+++ 220323 Урок по гитхабу/проверка гит/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace проверка_гит
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("проверка");
+            Console.WriteLine("проверка");
+
+        }
+    }
+}
