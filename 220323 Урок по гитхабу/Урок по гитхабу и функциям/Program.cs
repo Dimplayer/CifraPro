ae4c56154396cb60f2dd67a237f52f0ea935cc11
Dimplayer <Dimacoolin@gmail.com>
2023-03-24 18:34:41 +0200

    Тест проверка

---
+++ 220323 Урок по гитхабу/Урок по гитхабу и функциям/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Урок_по_гитхабу_и_функциям
+{
+    internal class Program
+    {
+        static void Main ( string[] args )
+        {
+            Console.WriteLine ("ПРоверка");
+
+        }
+    }
+}
