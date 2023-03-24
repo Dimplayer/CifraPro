19cf7ab25c4bd9346ab2845987abaf7d3485d846
Dimplayer <Dimacoolin@gmail.com>
2023-03-24 21:10:20 +0200

    определение данных

---
+++ 240323 разбираем домашки на паре/определения введенных данных/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+string _line;
+StringBuilder _valuestringBuilder = new StringBuilder();
+
+//ввод
+Console.WriteLine ("Введите значение: ");
+_line = Console.ReadLine ();
+
+//обработка
+for (int i = 0; i < _line.Length; i++)
+{
+    char letter = _line [i];
+    if(char.IsDigit(letter))
+    {
+        _valuestringBuilder.Append (letter); 
+    }//если число то вернет тру
+
+
+}
+
+//вывод
