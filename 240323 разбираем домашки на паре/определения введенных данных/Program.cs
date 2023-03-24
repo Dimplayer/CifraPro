0c50c0984705dcf683e479daad621beda04394e7
Dimplayer <Dimacoolin@gmail.com>
2023-03-24 21:19:35 +0200

    Update Program.cs

--- 240323 разбираем домашки на паре/определения введенных данных/Program.cs
+++ 240323 разбираем домашки на паре/определения введенных данных/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 
 string _line;
+float _value=0;
 StringBuilder _valuestringBuilder = new StringBuilder();
 
 //ввод
@@ -11,12 +12,26 @@ _line = Console.ReadLine ();
 for (int i = 0; i < _line.Length; i++)
 {
     char letter = _line [i];
-    if(char.IsDigit(letter))
-    {
-        _valuestringBuilder.Append (letter); 
-    }//если число то вернет тру
 
+    if (char.IsDigit ( letter ))
+    {
+        _valuestringBuilder.Append ( letter );
+    }//если число то вернет тру
+    else if (letter == '.' || letter == ',')
+    {
+        _valuestringBuilder.Append ( ',' );
+    }
 
 }
 
+if(_valuestringBuilder.Length == 0)
+{
+    Console.WriteLine ("Ошибка, нет чисел");
+}
+else
+{ 
+_value = float.Parse (_valuestringBuilder.ToString ());
+}
+
 //вывод
+Console.WriteLine ("Числа = "+ _value);
